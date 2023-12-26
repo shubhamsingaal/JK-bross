c1fc7d1a0ac71126a75726ff21b173a706f1ec62
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-12-26 13:32:07 +0530

    feat: Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
 import './App.css'
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import { LandingPage } from './pages';
 
 function App() {
 
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/verification-screen":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/loginsignup":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div>
-    <h1 className="text-3xl font-bold underline bg-primary">
-        Test Tailwind
-    </h1>
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      {/* <Route path="/verification-screen" element={<VerificationScreen />} /> */}
+      {/* <Route path="/loginsignup" element={<LoginSignup />} /> */}
+    </Routes>
   )
 }
 
