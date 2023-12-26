7bf5d95363fc5d097dfd6e712530e42ebc366c2b
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-12-26 14:09:40 +0530

    feat: react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,17 @@
 import './App.css'
-import {
-  Routes,
-  Route,
-  useNavigationType,
-  useLocation,
-} from "react-router-dom";
 import { LandingPage, SignUp, VerificationScreen } from './pages';
+import { BrowserRouter ,Route, Routes } from 'react-router-dom';
 
 function App() {
 
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
-
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action, pathname]);
-
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
-
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/verification-screen":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/loginsignup":
-        title = "";
-        metaDescription = "";
-        break;
-    }
-
-    if (title) {
-      document.title = title;
-    }
-
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
-
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/verification-screen" element={<VerificationScreen />} />
-      <Route path="/loginsignup" element={<SignUp />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/verification-screen" element={<VerificationScreen />} />
+        <Route path="/loginsignup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
