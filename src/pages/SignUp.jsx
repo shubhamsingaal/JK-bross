2522d3e4ddd0e054e2b8b9109dc0df3c441faf42
Shubham Kumar <shubhamkraj200@gmail.com>
2023-12-26 17:06:25 +0530

    assets added

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="flex flex-col md:flex-row h-screen">
+      {/* Section 1 */}
+      <div className="md:w-1/2 bg-blue-500 p-8 text-white h-full">
+        <h2 className="text-2xl font-bold mb-4">Section 1</h2>
+        <p>
+          This is the first section of the page. You can add your content here.
+        </p>
+      </div>
+
+      {/* Section 2 */}
+      <div className="md:w-1/2 bg-gray-200 p-8 h-full">
+        <h2 className="text-2xl font-bold mb-4">Section 2</h2>
+        <p>
+          This is the second section of the page. You can add more content here.
+        </p>
+      </div>
+    </div>
+
   )
 }
 
