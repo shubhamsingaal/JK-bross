91624313f0cb79203642d334c84bde9f51c6ea21
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-12-26 21:45:40 +0530

    fix: NAVBG fix

--- src/components/LandingPageNav.jsx
+++ src/components/LandingPageNav.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import jkbLogo from '../assets/jk-bro-logo.png'
-import navBg from "../assets/navBg.png"
 
 const LandingPageNav = () => {
   return (
-    <div className="relative flex items-center">
+    <div className="relative flex items-center h-[10svh] w-[100svw]">
         {/* Image is for test, need to remove once that curve on nav is implemented */}
-        <NavLink to="/">
-          <img src={navBg} className='bg-transparent'/>  
-        </NavLink>
         
         <div className="absolute w-[50svw] flex items-center justify-between">
-          <img src={jkbLogo} className='w-[5rem] ml-[10rem]'/>
+          <NavLink to="/">
+            <img src={jkbLogo} className='w-[5rem] ml-[10rem]'/>
+            </NavLink>
 
           <NavLink to="/enterprise" className="mr-[1rem] hover:font-semibold transition">  
             For Enterprise
