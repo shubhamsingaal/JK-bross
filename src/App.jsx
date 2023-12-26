e478a212888fc3b5d64e1030cb2e1d464887932a
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-12-26 12:53:04 +0530

    feat: Inital Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1 className="text-3xl font-bold underline bg-red-500">
+        Test Tailwind
+    </h1>
+      
+    </div>
+  )
+}
+
+export default App
