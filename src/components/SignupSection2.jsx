def966d41b07e4f51c277625ecaaa3ee8198e749
Shubham Kumar <shubhamkraj200@gmail.com>
2023-12-26 23:52:32 +0530

    Added section1

---
+++ src/components/SignupSection2.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const SignupSection2 = () => {
+    return (
+        <div className='flex-1 bg-gray-200 p-8'>
+            {/* Your content for SignupSection2 */}
+        </div>
+    );
+};
+
+export default SignupSection2;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
+import {SignupSection1, SignupSection2} from '../components'
 
 const SignUp = () => {
   return (
-    <div className="flex flex-col md:flex-row h-screen">
-      {/* Section 1 */}
-      <div className="md:w-1/2 bg-blue-500 p-8 text-white h-full">
-        <h2 className="text-2xl font-bold mb-4">Section 1</h2>
-        <p>
-          This is the first section of the page. You can add your content here.
-        </p>
-      </div>
-
-      {/* Section 2 */}
-      <div className="md:w-1/2 bg-gray-200 p-8 h-full">
-        <h2 className="text-2xl font-bold mb-4">Section 2</h2>
-        <p>
-          This is the second section of the page. You can add more content here.
-        </p>
-      </div>
-    </div>
+   <div className='flex'>
+    <SignupSection1/>
+    <SignupSection2/>
+   </div>
 
   )
 }
