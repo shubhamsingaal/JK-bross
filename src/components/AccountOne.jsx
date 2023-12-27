b8a96bd6e78b90e6678c929ea6e6c983c6aa8fd9
Shubham Kumar <shubhamkraj200@gmail.com>
2023-12-27 22:56:41 +0530

    added some external files

--- src/components/AccountOne.jsx
+++ src/components/AccountOne.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
-import worker from '../assets/worker.png';
-import logo from '../assets/layer-12@2x.png';
-import dots from '../assets/group-55@2x.png';
+import React from 'react';
+import logo from '/assets/layer-12@2x.png';
 
 const AccountOne = () => {
-  return (
-    <div>
-          <div className='flex-1 bg-white p-8 h-screen flex flex-col justify-center items-center'>
-              {/* Logo Image */}
-              <img
-                  src={logo}
-                  alt="Logo"
-                  className="w-36 h-auto mb-8"
-              />
+    const screenWidth = window.innerWidth;
 
-              {/* Cycle Image */}
-              <img
-                  src={worker}
-                  alt="Cycle Image"
-                  className="w-64 h-auto mb-8"
-              />
+    return (
+        <div>
+            <div className={`flex-1 bg-white p-8 ${screenWidth <= 768 ? 'h-auto' : 'h-screen'} flex flex-col justify-center items-center`} style={styles.container}>
+                {/* Logo Image */}
+                <img src={logo} alt="Logo" className="w-36 h-auto mb-8" />
 
-              <img
-                  src={dots}
-                  alt="Cycle Image"
-                  className="w-20 h-auto mb-8"
-              />
+                {/* Conditionally render Worker Image */}
+                {screenWidth > 768 && (
+                    <img src="/assets/worker.png" alt="Cycle Image" className="w-64 h-auto mb-8" />
+                )}
 
-              {/* Text Content */}
-              <p className="mb-4 text-center">
-                  This is the first section of the page. You can add your content here.
-              </p>
-              {/* Add additional text content as needed */}
-          </div>
-    </div>
-  )
-}
+                {/* Conditionally render Dots Image and Text */}
+                {screenWidth > 768 && (
+                    <>
+                        <img src="/assets/group-55@2x.png" alt="Cycle Image" className="w-20 h-auto mb-8" />
+                        {/* Text Content */}
+                        <p className="mb-4 text-center">
+                            This is the first section of the page. You can add your content here.
+                        </p>
+                    </>
+                )}
+               
+            </div>
+        </div>
+    );
+};
 
-export default AccountOne
+const styles = {
+    container: {
+       
+        boxSizing: 'border-box',
+
+        '@media (max-width: 768px)': {
+            padding: '16px',
+        },
+    },
+};
+
+export default AccountOne;
--- src/components/AccountTwo.jsx
+++ src/components/AccountTwo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import account from '../assets/Account.png';
- import '../css/AccountTwo.css'; // Import your CSS file
+import account from '/assets/Account.png';
+import '../css/AccountTwo.css';
 
 const AccountTwo = () => {
   const [name, setName] = useState('');
@@ -37,62 +37,74 @@ const AccountTwo = () => {
   };
 
   return (
-    <div className='flex-1 bg-gray-200 p-11 '>
-      <div className='container bg-white p-10   mb-12 rounded-lg h-full'>
+    <div className='flex-1 bg-gray-200 p-11 mx-px my-py '>
+      <div className='container bg-white p-10 mb-12 rounded-lg h-full'>
         <img src={account} alt='Logo' className='mx-5 mb-10' />
         <div className='mb-4'>
+         
           <input
             type='text'
-            placeholder='Name'
+            id='name'
+            placeholder='Enter your name'
             value={name}
             onChange={handleNameChange}
             className='input-field'
           />
+
           <input
             type='email'
-            placeholder='Email'
+            id='email'
+            placeholder='Enter your email'
             value={email}
             onChange={handleEmailChange}
             className='input-field'
           />
-          <div className='flex mb-2'>
-            <input
-              type='text'
-              placeholder='DD'
-              value={birthdate.day}
-              onChange={(e) => handleBirthdateChange('day', e.target.value)}
-              className='small-input-field'
-            />
-            <input
-              type='text'
-              placeholder='MM'
-              value={birthdate.month}
-              onChange={(e) => handleBirthdateChange('month', e.target.value)}
-              className='small-input-field'
-            />
-            <input
-              type='text'
-              placeholder='YY'
-              value={birthdate.year}
-              onChange={(e) => handleBirthdateChange('year', e.target.value)}
-              className='small-input-field'
-            />
+          <div className='birthdate-label'>
+            <label className='label-text'>Birthdate</label>
           </div>
+          <div className='flex mb-2'>
+            
+            <div className='flex small-input-container'>
+              <input
+                type='text'
+                placeholder='DD'
+                value={birthdate.day}
+                onChange={(e) => handleBirthdateChange('day', e.target.value)}
+                className='small-input-field'
+              />
+              <input
+                type='text'
+                placeholder='MM'
+                value={birthdate.month}
+                onChange={(e) => handleBirthdateChange('month', e.target.value)}
+                className='small-input-field'
+              />
+              <input
+                type='text'
+                placeholder='YY'
+                value={birthdate.year}
+                onChange={(e) => handleBirthdateChange('year', e.target.value)}
+                className='small-input-field'
+              />
+            </div>
+          </div>
+
+          <label htmlFor='purpose' className='label-text'>
+            I'm here for...
+          </label>
           <select
+            id='purpose'
             value={purpose}
             onChange={handlePurposeChange}
             className='input-field'
           >
-            <option value=''>I'm here for...</option>
+            <option value=''>Select</option>
             <option value='delivery'>Delivery</option>
             <option value='other'>Other services</option>
             {/* Add more options as needed */}
           </select>
         </div>
-        <button
-          onClick={handleSubmit}
-          className='submit-button'
-        >
+        <button onClick={handleSubmit} className='submit-button'>
           Submit
         </button>
       </div>
