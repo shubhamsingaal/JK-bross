21488d1f1d81ed30c9cf9822dabdfeb8749a521f
Shubham Kumar <shubhamkraj200@gmail.com>
2023-12-28 13:06:47 +0530

    verfication ui updated

--- src/components/VerificationTwo.jsx
+++ src/components/VerificationTwo.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import verification from '/assets/Verification.png';
+// import verification from '/assets/Verification.png';
 
 const VerificationTwo = () => {
     const [otp, setOtp] = useState(['', '', '', '']);
@@ -43,42 +43,43 @@ const VerificationTwo = () => {
     const isAllFieldsFilled = otp.every(value => value !== '');
 
     return (
-        <div className='flex-1 md:bg-gray-200 bg-white md:p-10  pt-1'>
-            <div className='container  mx-auto md:mb-2 md:mt-2 xl:mx-14 bg-white p-6  rounded-lg md:p-10 h-full w-auto'>
-                <img src={verification} alt='Logo' className='mx-4 mb-7 mt-7' />
+        <div className='flex-1 md:bg-gray-200 bg-white md:p-11 pt-1'>
+            <div className='container sm:p-10 mx-auto md:mb-2 md:mt-2 xl:mx-14 bg-white p-6  rounded-lg md:p-10 h-full w-auto'>
+                <h1 className='text-3xl font-extrabold text-textPrimary font-serif mb-10'>Verification</h1>
                 <div className='flex justify-between '>
                     <h4 className='mb-1 font-semibold'>+91 99999 99999</h4>
                     <button
                         onClick={handleChangeClick}
-                        className='text-sm no-underline text-blue-500 hover:text-blue-700 focus:outline-none focus:underline'
+                        className='text-sm no-underline text-blue-500 hover:text-blue-700 focus:outline-none '
                     >
                         Change
                     </button>
                 </div>
-                <p className='text-left mb-12 text-zinc-700 text-sm'>
+                <p className='text-left text-sm mb-12 lg:mb-1 text-zinc-400'>
                     One Time Password (OTP) has been sent to this number.
                 </p>
-                <div className='flex mb-2 justify-center'>
+                <div className='flex mb-1 lg:p-5 xl:p-10 gap-1 sm:gap-3 justify-center'>
                     {/* Four small boxes for OTP verification */}
                     {otp.map((value, index) => (
                         <input
                             key={index}
                             type='text'
+                            placeholder='_'
                             maxLength='1'
                             value={value}
                             onChange={(e) => handleOtpChange(index, e.target.value)}
                             onKeyDown={(e) => handleKeyDown(index, e)}
-                            className='flex-1  bg-gray-5  ml-1 w-1 h-auto p-2 border-2 border-[#D8E3FF] rounded-xl text-lg mb-6'
+                            className='flex-1 text-center bg-gray-4  ml-1 w-1  h-auto p-2 border-2 border-[#D8E3FF] rounded-xl text-lg mb-6'
                             ref={inputRefs.current[index]}
                         />
                     ))}
                 </div>
 
                 {/* "Change" and "Resend OTP" buttons */}
-                <div className='flex justify-between mt-2 mb-15'>
+                <div className='flex justify-between  '>
                     <button
                         onClick={handleResendClick}
-                        className='text-sm text-blue-500 hover:text-blue-700 focus:outline-none focus:underline'
+                        className='text-sm -mt-5 md:mb-9 text-blue-500 hover:text-blue-700 focus:outline-none '
                     >
                         Resend OTP
                     </button>
@@ -87,7 +88,7 @@ const VerificationTwo = () => {
                     {/* Submit button at the bottom */}
                     <button
                         onClick={handleSubmit}
-                        className={`w-full p-2 mt-12 relative  -bottom-20 rounded-3xl transition duration-300 ${isAllFieldsFilled ? 'bg-blue-500 text-white hover:bg-blue-600' : 'bg-blue-100 text-gray-600 cursor-not-allowed '}`}
+                        className={`w-full p-2 mt-2 md:mt-8 lg:mt-1 xl:mb-12 relative  -bottom-20 rounded-3xl transition duration-300 ${isAllFieldsFilled ? 'bg-blue-500 text-white hover:bg-blue-600' : 'bg-blue-100 text-gray-600 cursor-not-allowed '}`}
                         disabled={!isAllFieldsFilled}
                     >
                         Submit
