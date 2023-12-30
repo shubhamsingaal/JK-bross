8e70fa4f247edefa773f94eade14672166fa1831
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-12-30 20:27:10 +0530

    feat: cleanup

--- src/components/TrackOrders/ShippingInfo.jsx
+++ src/components/TrackOrders/ShippingInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import directionPins from '/assets/direction.svg'
 import { Button } from '../../ui';
+import VerticalShippingIndicator from './VerticalShippingIndicator';
 
 const steps = [
     { id: 1, value: 'Package dispatched', display: 'Dispatched', stepComplete: true },
@@ -9,49 +10,6 @@ const steps = [
     { id: 4, value: 'Vehicle has left', display: 'Vehicle Has Left', stepComplete: false },
     { id: 5, value: 'Unpacking and moving to location', display: 'Unpacking and Moving to Location', stepComplete: false },
   ];
-
-const VerticalShippingIndicator = ({ steps }) => {
-return (
-    <div className="flex items-center relative md:ml-[1.5rem]">
-        <div className="flex-grow relative">
-            {steps.map((step, index) => (
-            <div key={step.id} className="mb-6 flex items-center gap-[2rem]">
-                <div
-                className={`rounded-full w-5 h-5 ${
-                    step.stepComplete ? 'bg-primary border-4 border-outline' : 'bg-gray-300'
-                } mr-2`}
-                />
-                <div>
-                    <h4 className={`text-lg font-bold ${step.stepComplete ? 'text-primary' : 'text-gray-600'}`}>
-                        {step.display}
-                    </h4>
-                    <p className="text-sm text-gray-600">{step.value}</p>
-                </div>
-            </div>
-            ))}
-
-            {/* Dashed Line */}
-            {steps.length > 1 && (
-            <div className="absolute top-0 bottom-0 left-2 transform -translate-x-1/2">
-                {steps.map((step, index) => (
-                    <div
-                        key={step.id}
-                        className={`absolute top-4 left-0 w-0 h-[150px] ${
-                                step.stepComplete ? 'bg-primary' : 'bg-gray-300'
-                                    }`}
-                        style={{
-                                transform: `translateY(${index * (100 / (steps.length - 1))}%)`,
-                                borderLeft: step.stepComplete ? '2px solid transparent' : '2px dashed gray'
-                        }}
-                    />
-                ))}
-            </div>
-            )}
-        </div>
-    </div>
-);
-};
-
 
 const ShippingInfo = () => {
   return (
