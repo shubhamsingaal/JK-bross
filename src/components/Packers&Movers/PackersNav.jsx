5396646aa6ca9f3abbe22cdadd3eec9d82c30b27
Shubham Kumar <shubhamkraj200@gmail.com>
2023-12-30 10:40:44 +0530

    fix: PackersNav

--- src/components/Packers&Movers/PackersNav.jsx
+++ src/components/Packers&Movers/PackersNav.jsx
@@ -8,15 +8,15 @@ import ordersIcon from '/assets/ordericon.svg';
 
 const PackersNav = () => {
     return (
-        <div className='w-[100svw] h-[10svh] flex bg-[#F5F7FE] border-b-2 border-outline'>
-            <div className='w-[50svw] flex items-center justify-start md:ml-16'>
+        <div className='w-[100svw]  h-[10svh] flex bg-[#F5F7FE] border-b-2 border-outline '>
+            <div className='w-[50svw]  flex items-center justify-start md:ml-5 '>
                 <NavLink to="/">
-                    <img src={jkbLogo} className='md:w-[5rem] w-[4rem] ml-[1rem] md:ml-[10rem]' alt='Logo' />
+                    <img src={jkbLogo} className='md:w-[5rem] w-[4rem] ml-[1rem] md:ml-[2rem] lg:ml-[8rem]' alt='Logo' />
                 </NavLink>
             </div>
 
-            <div className='w-[50svw] flex items-center justify-center'>
-                <div className='flex gap-x-4 items-center text-textPrimary font-semibold'>
+            <div className='w-[150svw] lg:w-[50svw]  flex items-center justify-center'>
+                <div className='  flex gap-x-4  lg:gap-x-8 items-center text-textPrimary font-semibold'>
                     <NavLink to="/tracking" className='text-sm flex items-center'>
                         <img src={homeIcon} alt="Home Icon" className="w-4 h-4 mr-1" />
                         Home
