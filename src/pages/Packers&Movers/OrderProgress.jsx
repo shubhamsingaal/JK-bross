ada05b0452d81f7639136275694b6353d46836ec
Shubham Kumar <shubhamkraj200@gmail.com>
2023-12-30 12:21:14 +0530

    fix:orderprogress responsivenss

--- src/pages/Packers&Movers/OrderProgress.jsx
+++ src/pages/Packers&Movers/OrderProgress.jsx
@@ -13,43 +13,55 @@ const OrderDetails = () => {
         <div className='flex flex-col overflow-hidden'>
             <PackersNav />
 
-            <div className='flex mx-[60px] mb-2  mt-5 space-x-4'>
+            <div className='flex justify-start mb-2  mx-auto md:mx-12  mt-5 space-x-4'>
                 <TabButton
                     tab="All"
-                    count={0} // Replace with the actual count for "All"
+                    count={2} 
                     selected={selectedTab === 'All'}
                     onClick={() => handleTabClick('All')}
                 />
                 <TabButton
                     tab="Accepted"
-                    count={0} // Replace with the actual count for "Accepted"
+                    count={0} 
                     selected={selectedTab === 'Accepted'}
                     onClick={() => handleTabClick('Accepted')}
                 />
                 <TabButton
                     tab="Assigned"
-                    count={0} // Replace with the actual count for "Assigned"
+                    count={0} 
                     selected={selectedTab === 'Assigned'}
                     onClick={() => handleTabClick('Assigned')}
                 />
                 <TabButton
                     tab="Completed"
-                    count={0} // Replace with the actual count for "Completed"
+                    count={0} 
                     selected={selectedTab === 'Completed'}
                     onClick={() => handleTabClick('Completed')}
                 />
             </div>
 
-            <p className='mx-[60px]  mb-2 text-sm text-left text-gray-500'>On going</p>
+            
 
-            <div className='flex justify-evenly flex-wrap gap-1 md:flex-row flex-col mb-5'>
-                <OrderRequest />
-                <OrderRequest />
-                <OrderRequest />
-                <OrderRequest />
-                <OrderRequest />
-                <OrderRequest />
+            <div className='flex  mx-auto md:mx-7 lg:mx-12  gap-y-2 flex-wrap gap-2 md:flex-row lg:flex-col flex-col mb-5'>
+                <div className='section mt-7'>
+                    <p className='mb-2 text-sm  text-left text-gray-500'>Ongoing</p>
+                    <div className='flex flex-col md:flex-row md:flex-wrap md:gap-2'>
+                        <OrderRequest />
+                       
+                        
+                    </div>
+                </div>
+                
+                <div className='section mt-7 '>
+                    <p className='mb-2 text-sm text-left text-gray-500'>Accepted</p>
+                    <div className='flex flex-col md:flex-row md:flex-wrap md:gap-7'>
+                        <OrderRequest />
+                        <OrderRequest />
+                        <OrderRequest />
+                    </div>
+                </div>
             </div>
+
             <Footer />
         </div>
     );
@@ -62,7 +74,7 @@ const TabButton = ({ tab, count, selected, onClick }) => {
             onClick={onClick}
         >
             <span>{tab}</span>
-            {count > 0 && (
+            {count >= 0 && (
                 <span className="ml-1 text-xs bg-blue-500 text-white rounded-full px-2">
                     {count}
                 </span>
